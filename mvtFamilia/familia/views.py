9149c85ba48a99aa37f4629c0ac02db612499136
Luciano Vaimberg <luciano.vaimberg@gmail.com>
2022-08-21 13:41:25 -0300

    Trabajo finalizado V1

--- mvtFamilia/familia/views.py
+++ mvtFamilia/familia/views.py
@@ -8,19 +8,19 @@ from django.template import Context, Template
 def papa(request):
     pariente1=Parientes(nombre="Pepe", parentesco ="Padre", edad = "65", fecha=datetime.datetime.today())
     pariente1.save()
-    mensaje_papa=f"Mi nombre es {pariente1.nombre}, soy tu {pariente1.parentesco}, tengo {pariente1.edad} años y hoy es {pariente1.fecha}"
+    mensaje_papa=f"Mi nombre es <u><strong>{pariente1.nombre}</u></strong>, soy tu <strong>{pariente1.parentesco}</strong>, tengo <u>{pariente1.edad}</u> años <br><br> Fecha actual:<h3>{pariente1.fecha}</h3>"
     return HttpResponse(mensaje_papa)
 
 def mama(request):
     pariente2=Parientes(nombre="Pepa", parentesco ="Madre", edad = "61", fecha=datetime.datetime.today())
     pariente2.save()
-    mensaje_mama=f"Mi nombre es {pariente2.nombre}, soy tu {pariente2.parentesco}, tengo {pariente2.edad} años y hoy es {pariente2.fecha}"
+    mensaje_mama=f"Mi nombre es <u><strong>{pariente2.nombre}</strong></u>, soy tu <strong>{pariente2.parentesco}</strong>, tengo <u>{pariente2.edad}</u> años <br><br> Fecha actual:<h3>{pariente2.fecha}</h3>"
     return HttpResponse(mensaje_mama)    
 
 def hijo(request):
     pariente3=Parientes(nombre="Pepito", parentesco ="Hijo", edad = "11", fecha=datetime.datetime.today())
     pariente3.save()
-    mensaje_hijo=f"Mi nombre es {pariente3.nombre}, soy tu {pariente3.parentesco}, tengo {pariente3.edad} años y hoy es {pariente3.fecha}"
+    mensaje_hijo=f"Mi nombre es <u><strong>{pariente3.nombre}</strong></u>, soy tu <strong>{pariente3.parentesco}</strong>, tengo <u>{pariente3.edad}</u> años <br><br> Fecha actual:<h3>{pariente3.fecha}</h3>"
     return HttpResponse(mensaje_hijo) 
 
 def plantilla_html(request):
